1ef081e6d22e0e6c4d8c05add00785f28a1b6edc
Caffier Edouard <caffier.ed@gmail.com>
2021-05-02 23:29:10 +0200

    it dont work

---
+++ src/components/home/detail/Detail.js
@@ -0,0 +1,36 @@
+function Detail (props){
+    return (
+        <div className="col-12 col-sm-4 col-lg-3">
+            <article className="bg-dark text-center py-5">
+                <p>zz</p> 
+            </article>
+        </div>
+    )
+}
+export default Detail;
+
+
+
+// import Spaceship from '../detail/Spaceship'
+
+// function Detail(props) {
+//     return(
+
+//         <Spaceship>
+//         <div className="col-12 col-sm-4 col-lg-3">
+//             <article className="bg-dark text-center py-5 my-2">
+//                     <p>kkk</p> 
+//             </article>
+//         </div>
+//         </Spaceship>
+//     )
+// }
+// export default Detail;
+
+
+
+//         // <div className="col-12 col-sm-4 col-lg-3">
+//         //     <article className="bg-dark text-center py-5 my-2">
+//         //            <p>{props.list}</p>
+//         //     </article>
+//         // </div>
